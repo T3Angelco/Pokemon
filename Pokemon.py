a5abfc997e1f45167bdce65f891877307784cc13
Angel Corona <angelcoymisha2@outlook.com>
2023-05-21 16:27:22 -0600

    Fusioné 1 rama

--- Pokemon.py
+++ Pokemon.py
@@ -1,5 +1,5 @@
 pokemon = input ("Dime el nombre de un pokemon --> ")
-tipo = input ("Dame un número del 1 al 2 -- > ")
+tipo = input ("Dame un número del 1 al 2 --> ")
 if tipo == 1:
     print ("Tu pokemon es tipo agua")
 else:
