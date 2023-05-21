cffb15d4781ab7c79ce5a81d046f970c82b2a124
Angel Corona <angelcoymisha2@outlook.com>
2023-05-21 10:07:54 -0600

    Agregué el tipo de pokemon

--- Pokemon.py
+++ Pokemon.py
@@ -1,2 +1,8 @@
 pokemon = input ("Dime el nombre de un pokemon --> ")
+tipo = input ("Dame un número del 1 al 2 -- > ")
+if tipo == 1:
+    print ("Tu pokemon es tipo agua")
+else:
+    print ("Tu pokemon es tipo fuego")
+
 print (pokemon, "es un gran pokemón")
